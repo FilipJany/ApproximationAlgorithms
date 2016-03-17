10af96934de54796498758d02f16e2d4697c7570
Patryk Stopyra <patryk.stopyra@gmail.com>
2016-03-17 17:39:17 +0100

    new method push test: Graph & Vertex mocks.

---
+++ Lista 1/src/com/fjps/graph/Graph.java
@@ -0,0 +1,10 @@
+package com.fjps.graph;
+
+/**
+ * Class of whole graph.
+ *
+ * Created by MagnaT on 17/03/16.
+ */
+public class Graph {
+
+}
---
+++ Lista 1/src/com/fjps/graph/Vertex.java
@@ -0,0 +1,9 @@
+package com.fjps.graph;
+
+/**
+ * Class representing single node.
+ *
+ * Created by Patryk Stopyra on 17/03/16.
+ */
+public class Vertex {
+}
