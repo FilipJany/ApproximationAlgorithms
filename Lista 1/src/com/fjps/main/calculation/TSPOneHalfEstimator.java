f4cd4bac2c74d3dae47186d56433ff14bd597810
Patryk Stopyra <patryk.stopyra@gmail.com>
2016-03-28 17:06:57 +0200

    oddNeighbour generator.

--- Lista 1/src/com/fjps/main/calculation/TSPOneHalfEstimator.java
+++ Lista 1/src/com/fjps/main/calculation/TSPOneHalfEstimator.java
@@ -12,6 +12,9 @@ public class TSPOneHalfEstimator<T extends Number> implements TravellingSalesman
 
     @Override
     public T calculateOptimum(Graph<T> graph) {
+        Graph<T> mst = minimumSpanningTree(graph);
+
+
         return null;
     }
 
@@ -20,22 +23,49 @@ public class TSPOneHalfEstimator<T extends Number> implements TravellingSalesman
      * Result is kept as new <code>Graph</code> object containing the same nodes like the source graph and some of
      * original edges (those defining MST).
      *
-     * @param graph graph on which we are looking for a MST.
-     * @return New <code>Graph</code> object containing exactly the same vertex objects as <code>graph</code> and some
+     * @param original graph on which we are looking for a MST.
+     * @return New <code>Graph</code> object containing exactly the same vertex objects as <code>original</code> and some
      * subset of it's edges that defines minimum spanning tree.
      */
-    private Graph<T> minimumSpanningTree(Graph<T> graph) {
+    private Graph<T> minimumSpanningTree(Graph<T> original) {
         Graph<T> mst = new Graph<>();
 
-        graph.getAllVertexes().stream()
+        original.getAllVertexes().stream()
                 .map(Vertex::getID)
                 .forEach(mst::addVertex);
 
-        graph.getAllEdges().stream()
+        original.getAllEdges().stream()
                 .filter(e -> !mst.isConnected(e.getV1(), e.getV2()))
                 .peek(e -> mst.connect(e.getV1(), e.getV2(), e.getWeight()))
                 .anyMatch(e -> mst.getNumberEdges() == mst.getNumberVertexes() - 1);
 
         return mst;
+    }
+
+    /**
+     * Returns an indiced graph containing nodes with odd number of neighbours from <code>mst</code>.
+     * These nodes are connected with edges of weight (distance) took from the <code>original</code> graph,
+     * since there is no information about connection in <code>mst</code>.
+     *
+     * @param mst      graph containing minimum spanning tree to find odd-degree vertexes
+     * @param original original graph containing all edges information
+     * @return Graph made of odd-degree vertexes from <code>mst</code>,
+     * connected with edges from <code>original</code> graph.
+     */
+    private Graph<T> indicedOddNeighbourhood(Graph<T> mst, Graph<T> original) {
+        Graph<T> odd = new Graph<>();
+
+        mst.getAllVertexes().stream()
+                .filter(v -> v.getNeighbourhood().size() % 2 != 0)
+                .map(Vertex::getID)
+                .forEach(odd::addVertex);
+
+        for (Vertex<T> v : odd.getAllVertexes())
+            v.getNeighbourhood().keySet().stream()
+                    .filter(neighbour -> odd.hasVertex(v))
+                    .map(neighbour -> v.getEdge(neighbour))
+                    .forEach(edge -> odd.connect(edge.getV1(), edge.getV2(), edge.getWeight()));
+
+        return odd;
     }
 }
